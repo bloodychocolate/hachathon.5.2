9c0413cfae29a2eceeeb4d7203a50b9c54ad0e7b
t1meshift <tarakanama@mail.ru>
2016-09-24 16:41:26 +1000

    пилим пилим
    ВОТ ТАК БЫСТРО РАЗ РАЗ КОММИТЬ КОММИТЬ КОММИТЬ КОММИТЬ КОММИТЬ! КОММИТЬ!
    ГОВНО! КОММИТЬ!

--- Backend/api.php
+++ Backend/api.php
@@ -4,29 +4,33 @@ $db_login = 'host1512310';
 $db_pass = 'nope :^)';
 $db_name = 'host1512310_rt'
 ////////////////////////////
+///
+// http://host/api.php?login={LOGIN}&pass={PASS}&method={METHOD_NAME};
+
 function json_err($msg) {
 	die("{'error':'".addslashes($msg)."'");
 }
 
-if (!isset($_GET['login']) || !isset($_GET['pass']) || !isset($_GET['query']))
+if (!isset($_GET['login']) || !isset($_GET['pass']) || !isset($_GET['method']))
 	json_err("Incorrect request!");
 
 $login = mysql_real_escape_string($_GET['login']);
 $pass = mysql_real_escape_string($_GET['pass']);
-$query = $_GET['pass'];
+$method = $_GET['method'];
 
 if (!mysql_connect($db_host, $db_login, $db_pass) || !mysql_select_db($db_name))
 	json_err("Can't connect to DB!");
 
 $check_auth = mysql_query("SELECT * FROM $db_name WHERE login = '$login' AND pass = '$pass' LIMIT 1");
-if (mysql_num_rows($result) == 0)
+if (mysql_num_rows($check_auth) == 0)
 	json_err("Incorrect login or password!");
 
-switch ($query) {
+switch ($method) {
 	case 'getUserInfo':
 		if (!isset($_GET['id']))
 			json_err("Incorrect request!");
-		
+		$id = $_GET['id'];
+		$query = mysql_query("SELECT * FROM $db_name WHERE user_id = '$id' ")
 }
 
 ?>
