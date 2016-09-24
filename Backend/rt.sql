9e352b4703ad2d4b452a89a735d8d24831dc489e
t1meshift <tarakanama@mail.ru>
2016-09-25 01:53:50 +1000

    чота пилю
    я покакал

--- Backend/rt.sql
+++ Backend/rt.sql
@@ -8,7 +8,8 @@ CREATE TABLE users (
 	middle_name VARCHAR(40) NOT NULL,
 	income DECIMAL(15,2) NOT NULL,
 	xp INT UNSIGNED NOT NULL,
-	score INT UNSIGNED NOT NULL
+	new_clients INT UNSIGNED NOT NULL,
+	is_admin BOOLEAN NOT NULL
 );
 CREATE TABLE achievements (
 	a_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -22,10 +23,25 @@ CREATE TABLE user_achievements (
 	u_id INT UNSIGNED NOT NULL,
 	a_id INT UNSIGNED NOT NULL
 );
+CREATE TABLE teams (
+	team_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (team_id),
+	team_name VARCHAR(30) NOT NULL,
+	team_moder INT UNSIGNED NOT NULL
+);
+CREATE TABLE battles (
+	battle_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (battle_id),
+	sender INT UNSIGNED NOT NULL,
+	receiver INT UNSIGNED NOT NULL,
+	condition BOOLEAN NOT NULL
+);
+-- condition - 1 значит по доходу, 0 значит по новоиспечённым клиентам
 
-INSERT INTO users (user_id, login, password, first_name, last_name, middle_name, income, xp, score) 
+--4 тимы окда
+INSERT INTO users (user_id, login, password, first_name, last_name, middle_name, income, xp, new_clients) 
 VALUES ('1', 'root', 'root', 'Иван', 'Петров', 'Сидорович', '20000.00', '228', '1488');
-INSERT INTO users (login, password, first_name, last_name, middle_name, income, xp, score) 
+INSERT INTO users (login, password, first_name, last_name, middle_name, income, xp, new_clients) 
 VALUES ('hello', 'world', 'John', 'Doe', 'William', '25000.00', '322', '1337');
 
 INSERT INTO achievements (a_id, a_name, a_description)
