68c935c0aa3e722fe5996fb0cad5a3082538e29e
t1meshift <tarakanama@mail.ru>
2016-09-25 13:08:14 +1000

    говно

--- Backend/api.php
+++ Backend/api.php
@@ -11,7 +11,7 @@ function json_err($code, $msg) {
 	die("{'error':'".addslashes($msg)."','err_code':'$code'}");
 }
 function json_return($str) {
-	die('{"response":'.$str.'}')
+	die('{"response":'.$str.'}');
 }
 
 if (!isset($_GET['login']) || !isset($_GET['pass']) || !isset($_GET['method']))
@@ -96,11 +96,11 @@ switch ($method) {
 		$query = mysql_query("INSERT INTO battles (sender, receiver, end_date) VALUES ('$auth_id','$idd',now() + interval 1 month)");
 		json_return('"0"');
 		break;
-	case 'getQuests': 
+	case 'getQuest': 
 		$idd = $_GET['id'];
 		if (!isset($_GET['id'])) 
 			$idd = $auth_id; //ЕБУЧАЯ БЫДЛЯЦКАЯ ПЫХА!!!
-		$query = mysql_query("SELECT * FROM quests WHERE user_id = '$idd");
+		$query = mysql_query("SELECT * FROM quests WHERE user_id = '$idd'");
 		$rows = array();
 		while($r = mysql_fetch_assoc($query)) {
 		    $rows[] = $r;
@@ -108,7 +108,7 @@ switch ($method) {
 		json_return(json_encode($rows));
 	case 'addQuest':
 		if (!$is_admin)
-			json_err('Permission denied!');
+			json_err(2,'Permission denied!');
 		if (!isset($_GET['user']))
 			json_err(3,"$method must have the 'user' argument.");
 		if (!isset($_GET['brief']))
@@ -123,15 +123,30 @@ switch ($method) {
 		break;
 	case 'finishQuest':
 		if (!$is_admin)
-			json_err('Permission denied!');
+			json_err(2,'Permission denied!');
 		if (!isset($_GET['id']))
 			json_err(3,"$method must have the 'id' argument.");
 		$q_id = $_GET['id'];
-		$xp = mysql_result(mysql_query("SELECT xp FROM quests WHERE quest_id = '$q_id'"),0);
-		$query = mysql_query("UPDATE users SET 'xp' = 'xp'+$xp WHERE quest_id = '$q_id'");
+		$kal = mysql_query("SELECT xp, user_id FROM quests WHERE quest_id = '$q_id'");
+		$info = mysql_fetch_row($kal);
+		$xp = $info[0];
+		$uid = $info[1];
+		echo $xp. ' '. $uid;
+		$query = mysql_query("UPDATE users SET 'xp' = 'xp' + $xp WHERE user_id = '$uid'");
 		$query = mysql_query("DELETE FROM quests WHERE quest_id = '$q_id'");
 		json_return('"0"');
 		break;
+	case 'changeUserIncome':
+		if (!$is_admin)
+			json_err(2,'Permission denied!');
+		if (!isset($_GET['id']))
+			json_err(3,"$method must have the 'id' argument.");
+		if (!isset($_GET['income']))
+			json_err(3,"$method must have the 'income' argument.");
+		$id = $_GET['id'];
+		$zp = $_GET['income'];
+		$kal = mysql_query("UPDATE users SET income = '$zp' WHERE user_id = '$id'");
+		json_return('"0"');
 	default:
 		json_err(3,"Incorrect method name!");
 		break;
